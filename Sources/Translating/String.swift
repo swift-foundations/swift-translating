861f58764121a350dfc040d3682681487ae7137d
coenttb <22656652+coenttb@users.noreply.github.com>
2025-12-20 09:37:43 +0000

    Run swift-format

--- Sources/Translating/String.swift
+++ Sources/Translating/String.swift
@@ -226,7 +226,7 @@ extension String {
      Truncates the string to the specified length number of characters and appends an optional trailing string if longer.
      - Parameter length: Desired maximum lengths of a string
      - Parameter trailing: A 'String' that will be appended after the truncation.
-    
+
      - Returns: 'String' object.
      */
     public func trunc(length: Int, trailing: String = "…") -> String {
